f662e29214644547cc017018b3f7c2fd1903b23a
Muhammet eren Gür <muhammet.erengur@hotmail.com>
2025-04-20 02:20:47 +0300

    get script bugfix 2

--- api/ResponseSchemes/ScriptResponse.py
+++ api/ResponseSchemes/ScriptResponse.py
@@ -1,10 +1,14 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
+class Script(BaseModel):
+    id : int
+    title : str
+    topic : str
+    script : str
 
 class ScriptResponse(BaseModel):
     success : bool
     message : str
-    script : Optional[str] = None
-    scripts : Optional[list[str]] = None
+    script : Optional[Script] = None
+    scripts : Optional[list[Script]] = None
