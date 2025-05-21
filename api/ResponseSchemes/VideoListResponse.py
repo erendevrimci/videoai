e5378e5df027e09195e44477ca65310d60b3e3ec
Muhammet eren Gür <muhammet.erengur@hotmail.com>
2025-05-21 03:49:29 +0300

    orm fix

--- api/ResponseSchemes/VideoListResponse.py
+++ api/ResponseSchemes/VideoListResponse.py
@@ -1,12 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import List, Optional
 
 class VideoURLItem(BaseModel):
     created_at: Optional[str] = None
     url: Optional[str] = None
     id: Optional[int] = None
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
 
 class VideoListResponse(BaseModel):
     success: bool
