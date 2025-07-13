a2fde6b03966e69f27cbda90b4a970009947be03
erendevrimci <erendevrimci@gmail.com>
2025-07-13 14:24:43 +0300

    Delete api/db directory

--- api/db/supabase_config.py
+++
@@ -1,8 +0,0 @@
-import os
-from supabase import create_client, Client
-
-
-def get_supabase_client() -> Client:
-    supabase_url = os.getenv("SUPABASE_URL")
-    supabase_key = os.getenv("SUPABASE_KEY")
-    return create_client(supabase_url, supabase_key)
